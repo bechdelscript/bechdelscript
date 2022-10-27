4bef4673cb445e1d6cbcd6716f982fcefb78d44e
Lucie Clemot <lucie.clemot@student-cs.fr>
2022-10-27 11:46:53 +0200

    changed git structure

--- data_import.py
+++ data_import.py
@@ -10,14 +10,14 @@ import os
 
 
 def rename():
-    file_names = os.listdir('data/Scripts/')
+    file_names = os.listdir('data/inputs/scripts/')
 
     for file in file_names:
         if not file.startswith('.'):
             try:
                 with open(file) as f:
                     first_line = f.readline().strip()
-                    os.rename('data/Scripts/'+file, (str(first_line) + '.txt').upper())
+                    os.rename('data/inputs/scripts/'+file, (str(first_line) + '.txt').upper())
             except:
                 print(f'{file} could not be renamed. Please check manually')
 
@@ -27,7 +27,7 @@ def create_db():
     bechdel_df = pd.read_json('http://bechdeltest.com/api/v1/getAllMovies')
     print(bechdel_df.head())
     print(bechdel_df.shape)
-    bechdel_df.to_csv('../data/Bechdel_db.csv')
+    bechdel_df.to_csv('data/inputs/Bechdel_db.csv')
 
 #create_db()
 
