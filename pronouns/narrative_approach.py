251e3f5fed5a203aafd2faab1b67b14486ba4e30
Lucie Clemot <lucie.clemot@student-cs.fr>
2023-01-25 14:31:27 +0100

    added new gender method

--- pronouns/narrative_approach.py
+++ pronouns/narrative_approach.py
@@ -1,27 +1,22 @@
 import pandas as pd
 import nltk
-
-tokens = pd.read_csv("pronouns.csv", header=None)
-tokens = [token[0] for token in tokens.values]
-
-pronoun_to_gender = {
-    "he": "M",
-    "his": "M",
-    "him": "M",
-    "man": "M",
-    "she": "F",
-    "her": "F",
-    "hers": "F",
-    "woman": "F",
-    "they": "NB",
-    "them": "NB",
-    "their": "NB",
-    "theirs": "NB",
-}
+import os
+import yaml
 
 """Given a list of narrative passages, this function identifies the most frequently used gendered pronouns included in 
 paragraphs where the character's name appears. Then, we use the most frequent gender associated with narrative passages as our
 prediction."""
+
+def import_gender_tokens():
+    config = yaml.safe_load(open("parameters.yaml"))
+
+    path = os.path.join(
+        config["paths"]["pronoun_folder"],
+        config["names"]["list_pronoun_tokens"],
+    )
+
+    tokens = pd.read_csv(path, header=None, sep=";")
+    return tokens
 
 
 def pronoun_id(paragraphs, name, tokens):
@@ -30,25 +25,16 @@ def pronoun_id(paragraphs, name, tokens):
     for para in paragraphs:
         freq_tokens = {}
         for word in nltk.word_tokenize(para):
-            for token in tokens:
+            for token in tokens[0]:
                 if token == word.lower():
                     try:
                         freq_tokens[token] += 1
                     except:
                         freq_tokens[token] = 1
         freq_tokens = {
-            pronoun_to_gender[key]: freq_tokens[key] for key in freq_tokens.keys()
+            tokens.loc[tokens[0] == key][1].values[0]: freq_tokens[key]
+            for key in freq_tokens.keys()
         }
         freq_gender[max(freq_tokens, key=lambda k: freq_tokens[k])] += 1
     res = max(freq_gender, key=lambda k: freq_gender[k])
     return res
-
-
-paragraphs = [
-    "John is blabla. He is blabla. His hair. His daughter is also there. She is blabla.",
-    "John is a man.",
-    "Her hair falls down.",
-    "John is a she. She used...",
-]
-
-print(pronoun_id(paragraphs, "John", tokens))
