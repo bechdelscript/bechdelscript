4ece45127de93ba4d4a82785e2aa0f79bd346012
Lucie Clemot <lucie.clemot@student-cs.fr>
2023-03-08 15:09:36 +0100

    first script upload post method added

--- test-api/main.py
+++ test-api/main.py
@@ -1,23 +1,64 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
-from .. screenplay_classes import Script
+from screenplay_classes import Script
+import configue
+import chardet
+
+config = configue.load("parameters.yaml")
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.post("/upload_script/")
+async def upload_script(
+    file: UploadFile = File(...),
+):
+    filename = file.filename
+    if file:
+        content = await file.read()
+        content = content.decode("unicode_escape").replace("\r", "")
+
+        response = Script(content, config)
+        response.load_format()
+        response.bechdel()
+        score = response.computed_score
+        chars = response.list_characters
+        return {
+            "message": "Fichier {} lu".format(filename),
+            "Score calculé": score,
+            "Personnages": chars,
+        }
+    else:
+        return {"message": "There was an error uploading the file {}".format(filename)}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# /
+@app.get("/")
+async def home():
+    return {"Message": "Welcome to the Bechdel Script Tester"}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+# /list-scripts
+@app.get("/list-scripts")
+async def list_scripts():
+    return {"Movies in base": []}
+
+
+# c'est ici qu'il faut mettre la base des films. tous les titres?
+
+# /result-by-title
+@app.get("/result-by-title/{filename}")
+async def result_by_title(filename: str):
+    # if filename dans le dossier
+    # return {"result" : score quand on crée le script}
+    # gérer cas où ça marche pas : raise HTTPException(404, f"Movie not in base")
+    pass
+
+
+# /add-script
+@app.post("/add-script")
+async def add_script(filename: str):
+    # db.append(filename)
+    return {"message": f"added new script {filename} to DB"}
