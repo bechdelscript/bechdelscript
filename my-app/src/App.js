36fa9f27006d691d0becd233a08cbbf182e6c05f
Guilhem Prince <guilhem.prince@student-cs.fr>
2023-03-09 11:00:16 +0100

    add box and conditionnal show for characters_list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,40 +4,45 @@ import './App.css';
 import CharacterList from './components/characters_list';
 import FileUpload from './components/file_upload';
 
+// const obj = null;
 const obj = {
-  "characters": [{
-    "name": "marge",
-    "gender": "female"
-  },
-  {
-    "name": "homer",
-    "gender": "male"
-  },
-  {
-    "name": "lisa",
-    "gender": "female"
-  }]
+	"characters": [{
+		"name": "marge",
+		"gender": "female"
+	},
+	{
+		"name": "homer",
+		"gender": "male"
+	},
+	{
+		"name": "lisa",
+		"gender": "female"
+	}]
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          <div>Bechdel Test AI</div>
-        </p>
-      </header>
-      <body>
-        <div>
-          <FileUpload />
-        </div>
-        <div>
-          <CharacterList characters={obj.characters} />
-        </div>
-      </body>
-    </div>
-  );
+	let character_list;
+	if (obj) { character_list = <CharacterList characters={obj.characters} /> }
+	else { character_list = null }
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				{/* <img src={logo} className="App-logo" alt="logo" /> */}
+				<p>
+					<div>Bechdel Test AI</div>
+				</p>
+			</header>
+			<body>
+				<div>
+					<FileUpload />
+				</div>
+				<div>
+					{character_list}
+				</div>
+			</body>
+		</div>
+	);
 }
 
 export default App;
