4c5c1ac5c92f9c452ed0714834090ac6eddf406c
Guilhem Prince <guilhem.prince@student-cs.fr>
2023-03-22 17:22:20 +0100

    change structure

--- topic_modeling/utils.py
+++ topic_modeling/utils.py
@@ -1,3 +1,16 @@
+import json
+import os
+
+
+def import_masculine_words(config):
+    path_masculine_words = os.path.join(
+        config["paths"]["topic_modeling_folder"],
+        config["names"]["list_of_words_about_men"],
+    )
+    masculine_words = json.load(open(path_masculine_words))
+    return masculine_words
+
+
 def clean_text(string, strip=True, add_spaces_to_extremities=False):
     if strip:
         string = string.strip()
