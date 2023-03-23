fd0ce730b954e1a0be27a8c11e9c06bfdbc47385
Guilhem Prince <guilhem.prince@student-cs.fr>
2023-03-23 15:21:04 +0100

    start writing some info

--- front_end/src/components/information_box.js
+++ front_end/src/components/information_box.js
@@ -5,15 +5,25 @@ export default function InformationBox() {
 
     return (
         <>
+            <b>TO DO : reduce spaces above and below container. Pimp the look inside container.</b>
             <div className="information-box-container">
-                <b>Welcome to the Bechdel Script Tester !</b>
+                <b>Welcome to the Bechdel Script Tester !</b> <br />
+                TO DO : phrase d'intro. Automated tester. Not really perfect, at all levels. Tool openly available to everyone, transparency. Github : TO DO LINK.
                 <div className="left-align-text">
-                    A movie passes the Bechdel test if :
-                    <ol>
-                        <li>There are two named female characters</li>
-                        <li>They talk with each other</li>
-                        <li>About something other than a man</li>
-                    </ol>
+                    Upload Section<br />
+                    TO DO : Describe the format of a script
+                </div>
+                <div className="left-align-text">
+                    Parameters Section<br />
+                    TO DO : What do they mean ? Describe the 3 possible configurations.
+                </div>
+                <div className="left-align-text">
+                    Score and Result Section <br />
+                    See the score out of the three criteria, and if possible, see the scenes (and highlighted dialogues) that validate the score.
+                </div>
+                <div className="left-align-text">
+                    Characters Section<br />
+                    Allow you to see which characters are identified by our parser, and how they were gendered. In case of an error, you can modify a character's gender and test the script again with the updated genders.
                 </div>
             </div>
         </>
