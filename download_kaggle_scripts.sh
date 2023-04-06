63d59978b3d4760b45ef6c9694fe7be0b28990df
Sacha Muller <chloe.muller@student-cs.fr>
2023-04-06 15:56:31 +0200

    fix: make kaggle download script executable

--- download_kaggle_scripts.sh
+++ download_kaggle_scripts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-fileid="16Ae_Dqz7RjFTXc7reiNvnHpXZ3jfkQhg"
-filename="back_end/data/input/"
+fileid="1FfQDJRqHQsQPxw-lWUw_4ja30lWHbbSD"
+filename="back_end/data/input/scripts_kaggle.zip"
+destination_folder="back_end/data/input/"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
 rm ./cookie
+unzip ${filename} -d ${destination_folder}
+rm ${filename}
